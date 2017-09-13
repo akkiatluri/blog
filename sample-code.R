d1ac28d55ec40bd872af78333237a71d6f06ad01
Akhilesh Atluri <aatlur87@lakers.mercyhurst.edu>
2017-09-13 12:53:40 -0400

    title change

--- sample-code.R
+++ sample-code.R
@@ -4,3 +4,8 @@ library(blogdown)
 install_hugo()
 new_site(theme = "calintat/minimal")
 build_site()
+
+
+serve_site() # to run the site locally after you make changes in config file
+
+new_post("test") # to create a new post
